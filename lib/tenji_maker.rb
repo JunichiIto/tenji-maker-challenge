ecbe30f596298a1799022776a463e6353cee7cd0
adebadayo <adebayanen@gmail.com>
2021-11-03 00:42:45 +0900

    リファクタリング

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -1,44 +1,18 @@
+require 'tenji_block'
+
 class TenjiMaker
+  # 点字関連の定数
+  include TenjiBlock
+
   def to_tenji(text)
-    tenji_blocks = text.split.map do |char|
-      tenji_block(char)
-    end
-    format_tenji(tenji_blocks)
-  end
-
-  # 文字を引数に点字情報をHashに格納する
-  # @param [String] ローマ字
-  # @return [Hash]
-  # ti(ち)
-  # - o
-  # - -
-  # - o
-  #     {
-  #       1 => true, 2 => false,
-  #       3 => true, 4 => true,
-  #       5 => true, 6 => false
-  #     }
-  def tenji_block(char)
-    if OTHER.keys.include?(char)
-      OTHER[char]
-    else
-      # 子音
-      shiin = char.length == 1 ? 'a' : char[0..-2]
-      # 母音
-      boin = char[-1]
-      BOIN[boin].merge(SHIIN[shiin])
-    end
-  end
-
-  #　tenji_blockの配列から点字フォーマットの文字列を作成
-  # @param [Array] tenji_blockを要素とする配列
-  def format_tenji(tenji_blocks)
     lines = {
       first: [],
       second: [],
       third: []
     }
-    tenji_blocks.each do |tb|
+
+    text.split.map do |char|
+      tb = tenji_block(char)
       lines[:first] << "#{tb[1]}#{tb[4]}"
       lines[:second] << "#{tb[2]}#{tb[5]}"
       lines[:third] << "#{tb[3]}#{tb[6]}"
@@ -51,99 +25,42 @@ class TenjiMaker
     TENJI
   end
 
+  # 文字を引数に点字情報をHashに格納する
+  # @param [String] ローマ字('HA', 'N', etc..)
+  # @return [Hash]
+  # e.g.) ti(ち)
+  # {
+  #   1 => 'o', 4 => '-',
+  #   2 => 'o', 5 => 'o',
+  #   3 => 'o', 6 => '-',
+  #  }
+  def tenji_block(char)
 
-  OTHER = {
-    'YA' => {
-      1 => '-', 4 => 'o',
-      2 => '-', 5 => '-',
-      3 => 'o', 6 => '-',
-    },
-    'YU' => {
-      1 => '-', 4 => 'o',
-      2 => '-', 5 => '-',
-      3 => 'o', 6 => 'o',
-    },
-    'YO' => {
-      1 => '-', 4 => 'o',
-      2 => '-', 5 => 'o',
-      3 => 'o', 6 => '-',
-    },
-    'WA' => {
-      1 => '-', 4 => '-',
-      2 => '-', 5 => '-',
-      3 => 'o', 6 => '-',
-    },
-    'WO' => {
-      1 => '-', 4 => '-',
-      2 => '-', 5 => 'o',
-      3 => 'o', 6 => '-',
-    },
-    'N' => {
-      1 => '-', 4 => '-',
-      2 => '-', 5 => 'o',
-      3 => 'o', 6 => 'o',
-    }
-  }
+    if TenjiBlock::UNIQUE_TENJI.keys.include?(char)
+      # 規則性がない文字の場合
+      TenjiBlock::UNIQUE_TENJI[char]
+    else
+      # 規則性がある文字の場合
 
-  # ① ④
-  # ②
-  BOIN = {
-    'A' => {
-      1 => 'o', 4 => '-',
-      2 => '-'
-    },
-    'I' => {
-      1 => 'o', 4 => '-',
-      2 => 'o'
-    },
-    'U' => {
-      1 => 'o', 4 => 'o',
-      2 => '-'
-    },
-    'E' => {
-      1 => 'o', 4 => 'o',
-      2 => 'o'
-    },
-    'O' => {
-      1 => '-', 4 => 'o',
-      2 => 'o'
-    },
-  }
+      # 初期値(全て'-')
+      tenji = TenjiBlock::BLANK_TENJI.dup
 
-  #    ⑤
-  # ③ ⑥
-  SHIIN = {
-    'a' => {
-                5 => '-',
-      3 => '-', 6 => '-'
-    },
-    'K' => {
-                5 => '-',
-      3 => '-', 6 => 'o'
-    },
-    'S' => {
-                5 => 'o',
-      3 => '-', 6 => 'o'
-    },
-    'T' => {
-                5 => 'o',
-      3 => 'o', 6 => '-'
-    },
-    'N' => {
-                5 => '-',
-      3 => 'o', 6 => '-'
-    },
-    'H' => {
-                5 => '-',
-      3 => 'o', 6 => 'o'
-    },
-    'M' => {
-                5 => 'o',
-      3 => 'o', 6 => 'o'
-    },
-    'R' => {
-                5 => 'o',
-      3 => '-', 6 => '-'
-    },
-  }
+      # 母音
+      # 末尾の文字が母音
+      vowel_text = char[-1]
+      # 母音の点字情報をマージ
+      tenji.merge!(TenjiBlock::VOWEL_TENJI[vowel_text])
+
+      # 子音
+      if char.length > 1
+        # 2文字以上の場合は末尾の文字だけ除けば子音となる
+        consonant_text = char[0..-2]
+        # 子音の点字情報をマージ
+        tenji.merge!(TenjiBlock::CONSONANT_TENJI[consonant_text])
+      end
+
+      tenji
+    end
+  end
+
 end
