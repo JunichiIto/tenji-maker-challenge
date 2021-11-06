d26d3d88f1894473ac05f376594bf380f01d537e
shuntagami <shun.tagami@i-nations.tokyo>
2021-11-06 17:09:47 +0900

    リファクタリング

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -2,105 +2,68 @@ class TenjiMaker
   DOT = 'o'.freeze
   DASH = '-'.freeze
 
+  Point = Struct.new(:i, :j)
+  ZERO_ZERO, ZERO_ONE = Point.new(0, 0), Point.new(0, 1)
+  ONE_ZERO, ONE_ONE = Point.new(1, 0), Point.new(1, 1)
+  TWO_ZERO, TWO_ONE = Point.new(2, 0), Point.new(2, 1)
+
   def to_tenji(text)
-    result = []
-    text.split(' ').each do |s|
+    result = text.split.map do |s|
       /(?<consonant>[KSTNHMYRW]|)(?<vowel>[AIUEO]|)/ =~ s
-      initial_state = three_row_two_column
-      consonant && assign_consonant(initial_state, consonant, vowel)
-      vowel && assign_vowel(initial_state, consonant, vowel)
-      result << initial_state
+      tenji_unit = [
+        [DASH, DASH],
+        [DASH, DASH],
+        [DASH, DASH]
+      ]
+      !consonant.empty? && \
+        _points_consonant(consonant, vowel)&.each { |point| tenji_unit[point.i][point.j] = DOT }
+      !vowel.empty? && \
+        _points_vowel(consonant, vowel).each { |point| tenji_unit[point.i][point.j] = DOT }
+      tenji_unit
     end
-    matrix_to_string(result)
+    _to_string(result)
   end
 
   private
 
-  # 出力する点字の最小単位
-  # --
-  # --
-  # --
-  # を 3 x 2の行列とみなす
-  def three_row_two_column
-    # [0][0][0][1]
-    # [1][0][1][1]
-    # [2][0][2][1]
-    Array.new(3) { Array.new(2, DASH) }
-  end
-
-  def assign_consonant(initial_state, consonant, vowel)
+  def _points_consonant(consonant, vowel)
     case consonant
-    when 'K'
-      initial_state[2][1] = DOT
-    when 'S'
-      initial_state[1][1] = DOT
-      initial_state[2][1] = DOT
-    when 'T'
-      initial_state[2][0] = DOT
-      initial_state[1][1] = DOT
-    when 'N'
-      initial_state[2][0] = DOT
-      if vowel.empty?
-        initial_state[1][1] = DOT
-        initial_state[2][1] = DOT
-      end
-    when 'H'
-      initial_state[2][0] = DOT
-      initial_state[2][1] = DOT
-    when 'M'
-      initial_state[2][0] = DOT
-      initial_state[1][1] = DOT
-      initial_state[2][1] = DOT
-    when 'Y'
-      initial_state[0][1] = DOT
-    when 'R'
-      initial_state[1][1] = DOT
-    when 'W'
-      # NOP
+    when 'K'; [TWO_ONE]
+    when 'S'; [ONE_ONE, TWO_ONE]
+    when 'T'; [ONE_ONE, TWO_ZERO]
+    when 'N'; vowel.empty? ? [ONE_ONE, TWO_ZERO, TWO_ONE] : [TWO_ZERO]
+    when 'H'; [TWO_ZERO, TWO_ONE]
+    when 'M'; [ONE_ONE, TWO_ZERO, TWO_ONE]
+    when 'Y'; [ZERO_ONE]
+    when 'R'; [ONE_ONE]
+    when 'W'; nil
     end
   end
 
-  def assign_vowel(initial_state, consonant, vowel)
+  def _points_vowel(consonant, vowel)
     if %w[W Y].include?(consonant)
       case vowel
-      when 'A'
-        initial_state[2][0] = DOT
-      when 'U'
-        initial_state[2][0] = DOT
-        initial_state[2][1] = DOT
-      when 'O'
-        initial_state[1][1] = DOT
-        initial_state[2][0] = DOT
+      when 'A'; [TWO_ZERO]
+      when 'U'; [TWO_ZERO, TWO_ONE]
+      when 'O'; [ONE_ONE, TWO_ZERO]
       end
     else
       case vowel
-      when 'A'
-        initial_state[0][0] = DOT
-      when 'I'
-        initial_state[0][0] = DOT
-        initial_state[1][0] = DOT
-      when 'U'
-        initial_state[0][0] = DOT
-        initial_state[0][1] = DOT
-      when 'E'
-        initial_state[0][0] = DOT
-        initial_state[0][1] = DOT
-        initial_state[1][0] = DOT
-      when 'O'
-        initial_state[0][1] = DOT
-        initial_state[1][0] = DOT
+      when 'A'; [ZERO_ZERO]
+      when 'I'; [ZERO_ZERO, ONE_ZERO]
+      when 'U'; [ZERO_ZERO, ZERO_ONE]
+      when 'E'; [ZERO_ZERO, ZERO_ONE, ONE_ZERO]
+      when 'O'; [ZERO_ONE, ONE_ZERO]
       end
     end
   end
 
-  def matrix_to_string(result)
-    line1 = ''
-    line2 = ''
-    line3 = ''
-    result.each do |x|
-      line1 += "#{x[0][0]}#{x[0][1]} "
-      line2 += "#{x[1][0]}#{x[1][1]} "
-      line3 += "#{x[2][0]}#{x[2][1]} "
+  def _to_string(result)
+    line1, line2, line3 = '', '', ''
+    result.each do |tenji_unit|
+      line1 += "#{tenji_unit[0][0]}#{tenji_unit[0][1]} "
+      line2 += "#{tenji_unit[1][0]}#{tenji_unit[1][1]} "
+      line3 += "#{tenji_unit[2][0]}#{tenji_unit[2][1]} "
     end
     "#{line1.strip}\n#{line2.strip}\n#{line3.strip}"
   end
