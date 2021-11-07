a3934c3d0580068d6989a5ccd07a2406b3ab9ab1
Paru871 <mahalo0521@gmail.com>
2021-11-07 19:55:29 +0900

    第1版を作成しました

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -1,43 +1,55 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
 class TenjiMaker
+  NORMAL_LETTERS = {
+    'A' => [0],
+    'I' => [0, 2],
+    'U' => [0, 1],
+    'E' => [0, 1, 2],
+    'O' => [1, 2],
+    'K' => [5],
+    'S' => [3, 5],
+    'T' => [3, 4],
+    'N' => [4],
+    'H' => [4, 5],
+    'M' => [3, 4, 5],
+    'R' => [3]
+  }.freeze
+
+  SPECIAL_LETTERS = {
+    'WA' => [4],
+    'YA' => [1, 4],
+    'YU' => [1, 4, 5],
+    'YO' => [1, 3, 4],
+    'NN' => [3, 4, 5]
+  }.freeze
+
   def to_tenji(text)
-    # 以下はサンプルの仮実装なので、このcase文は全部消して自作ロジックに書き直すこと
-    case text
-    when 'A HI RU'
-      <<~TENJI.chomp
-        o- o- oo
-        -- o- -o
-        -- oo --
-      TENJI
-    when 'KI RI N'
-      <<~TENJI.chomp
-        o- o- --
-        o- oo -o
-        -o -- oo
-      TENJI
-    when 'SI MA U MA'
-      <<~TENJI.chomp
-        o- o- oo o-
-        oo -o -- -o
-        -o oo -- oo
-      TENJI
-    when 'NI WA TO RI'
-      <<~TENJI.chomp
-        o- -- -o o-
-        o- -- oo oo
-        o- o- o- --
-      TENJI
-    when 'HI YO KO'
-      <<~TENJI.chomp
-        o- -o -o
-        o- -o o-
-        oo o- -o
-      TENJI
-    when 'KI TU NE'
-      <<~TENJI.chomp
-        o- oo oo
-        o- -o o-
-        -o o- o-
-      TENJI
+    text_array = text.split.map(&:chars)
+    braille_array = conversion(text_array).map do |element|
+      braille_text = +'------'
+      element.each do |n|
+        braille_text[n] = 'o'
+      end
+      braille_text.scan(/.{1,2}/)
+    end
+    braille_array.transpose.map { |a| a.join(' ') }.join("\n")
+  end
+
+  def conversion(text_array)
+    text_array.map do |first, second|
+      second ||= first
+      if SPECIAL_LETTERS.key?(first + second)
+        SPECIAL_LETTERS[first + second]
+      else
+        NORMAL_LETTERS[first] | NORMAL_LETTERS[second]
+      end
     end
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  input = ARGV[0]
+  puts TenjiMaker.new.to_tenji(input)
 end
