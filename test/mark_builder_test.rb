ebc1960f73e7dc73a4afe31ec49c2b83e2d23b28
suzuki-mar <supasu145@gmail.com>
2021-11-22 16:41:28 +0900

    Tenji Maker created

---
+++ test/mark_builder_test.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../lib/tenji'
+require_relative '../lib/mark_builder'
+
+class MarkBuilderTest < Minitest::Test
+  def setup
+    @builder = MarkBuilder.new
+  end
+
+  def test_build_tenjis_A_HI_RU
+    tenjis = build_tenjis('A HI RU')
+
+    assert_equal <<~TENJI.chomp, @builder.builds(tenjis)
+      o- o- oo
+      -- o- -o
+      -- oo --
+    TENJI
+  end
+
+  def test_build_tenjis_HI_YO_KO
+    tenjis = build_tenjis('HI YO KO')
+
+    assert_equal <<~TENJI.chomp, @builder.builds(tenjis)
+      o- -o -o
+      o- -o o-
+      oo o- -o
+    TENJI
+  end
+
+  private
+
+  def build_tenjis(string)
+    chars = string.split(MarkBuilder::DELMITER)
+    chars.map { |char| Tenji.new(char) }
+  end
+end
