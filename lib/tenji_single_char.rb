7d43ef2e53c6030777fa5a2eb630208010adf76e
watame <pandora.border@gmail.com>
2021-11-30 22:50:25 +0900

    fix: fix case pattern

--- lib/tenji_single_char.rb
+++ lib/tenji_single_char.rb
@@ -6,22 +6,22 @@ class TenjiSingleChar
   def to_tenji_single_array(char)
     template = Array.new(3) { Array.new(2, '-') }
     case char
-    when 'A'
+    when 'A' then
       template[0][0] = 'o'
       template
-    when 'I'
+    when 'I' then
       template[1][0] = 'o'
       merge_tenji_array(to_tenji_single_array('A'), template)
-    when 'U'
+    when 'U' then
       template[0][1] = 'o'
       merge_tenji_array(to_tenji_single_array('A'), template)
-    when 'E'
+    when 'E' then
       merge_tenji_array(to_tenji_single_array('I'), to_tenji_single_array('U'))
-    when 'O'
+    when 'O' then
       template[0][1] = 'o'
       template[1][0] = 'o'
       template
-    when 'N'
+    when 'N' then
       # Eの配列を上下反転、左右反転させる
       # https://mebee.info/2020/12/15/post-23548/
       to_tenji_single_array('E').reverse.map(&:reverse)
