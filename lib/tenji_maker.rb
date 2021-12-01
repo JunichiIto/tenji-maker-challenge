6bc116efc3a3813e0638543da20a7dd708cfe9b1
watame <pandora.border@gmail.com>
2021-12-01 23:51:51 +0900

    update: 点字文章作成関数を更新

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -1,43 +1,17 @@
+require_relative '../lib/tenji_char_generator'
+
 class TenjiMaker
-  def to_tenji(text)
-    # 以下はサンプルの仮実装なので、このcase文は全部消して自作ロジックに書き直すこと
-    case text
-    when 'A HI RU'
-      <<~TENJI.chomp
-        o- o- oo
-        -- o- -o
-        -- oo --
-      TENJI
-    when 'KI RI N'
-      <<~TENJI.chomp
-        o- o- --
-        o- oo -o
-        -o -- oo
-      TENJI
-    when 'SI MA U MA'
-      <<~TENJI.chomp
-        o- o- oo o-
-        oo -o -- -o
-        -o oo -- oo
-      TENJI
-    when 'NI WA TO RI'
-      <<~TENJI.chomp
-        o- -- -o o-
-        o- -- oo oo
-        o- o- o- --
-      TENJI
-    when 'HI YO KO'
-      <<~TENJI.chomp
-        o- -o -o
-        o- -o o-
-        oo o- -o
-      TENJI
-    when 'KI TU NE'
-      <<~TENJI.chomp
-        o- oo oo
-        o- -o o-
-        -o o- o-
-      TENJI
-    end
+
+  def initialize 
+    @tenji_generator = TenjiCharGenerator.new
   end
+
+  def to_tenji(text)
+    romaji_char_array = text.split(' ')
+    tenji_array = text.split(' ').map { |romaji_char|
+      @tenji_generator.to_tenji_char(romaji_char)
+    }
+    @tenji_generator.show_tenji(tenji_array)
+  end
+
 end
