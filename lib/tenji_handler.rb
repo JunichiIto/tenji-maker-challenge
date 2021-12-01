8f0b5e39aaa04c6aa8956d6975c332f7f55d1ddc
watame <pandora.border@gmail.com>
2021-12-01 23:51:23 +0900

    update: 点字表示関数を更新

--- lib/tenji_handler.rb
+++ lib/tenji_handler.rb
@@ -1,6 +1,22 @@
 module TenjiHandler
+
   def show_tenji(tenji_array)
-    tenji_array.map{|tenji_row| tenji_row.join{""}}.join("\n")
+    tenji_rows = []
+    # 点字は3段なのでそれぞれの行で取得
+    0.upto(2) do |num|
+      # 点字1文字の配列と、それ以外で分岐
+      if tenji_array[0][0].kind_of?(String)
+        tenji_row = tenji_array[num].join('')
+      else
+        tenji_row = tenji_array.inject(''){|tenji_result_str, tenji_part| 
+          # それぞれの配列を文字として取得し、区切り文字として半角スペースを入れる
+          tenji_result_str += tenji_part[num].join('') + ' '
+        }
+      end
+      tenji_rows << tenji_row
+    end
+
+    tenji_rows.map{ |tenji_row| tenji_row.strip }.join("\n")
   end
 
   def merge_tenji_array(*array)
