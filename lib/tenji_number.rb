e4928784c80b736262bfce7e6e3b15c672d8ef2a
emiko <31835314+siroemk@users.noreply.github.com>
2021-12-02 16:51:28 +0900

    TenjiNumberクラスに定数を追加した

--- lib/tenji_number.rb
+++ lib/tenji_number.rb
@@ -1,7 +1,33 @@
 # frozen_string_literal: true
 
 class TenjiNumber
-  def initialize(letter)
-    @letter = letter
+  VOWEL_NUMBER = {
+    'A' => [1],
+    'I' => [1, 2],
+    'U' => [1, 4],
+    'E' => [1, 2, 4],
+    'O' => [2, 4]
+  }.freeze
+
+  CONSONANT_NUMBER = {
+    'K' => [6],
+    'S' => [5, 6],
+    'T' => [3, 5],
+    'N' => [3],
+    'H' => [3, 6],
+    'M' => [3, 5, 6],
+    'R' => [5]
+  }.freeze
+
+  OTHER_LETTER_NUMBER = {
+    'YA' => [3, 4],
+    'YU' => [3, 4, 6],
+    'YO' => [3, 4, 5],
+    'WA' => [3],
+    'N' => [3, 5, 6]
+  }.freeze
+
+  def initialize(letters)
+    @letters = letters
   end
 end
