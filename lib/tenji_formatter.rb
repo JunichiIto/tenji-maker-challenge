d5706ba69503b5c4c72d2d8f72bc627ba14e335a
emiko <31835314+siroemk@users.noreply.github.com>
2021-12-02 16:59:53 +0900

    TenjiFormatterクラスにメソッドを追加した

--- lib/tenji_formatter.rb
+++ lib/tenji_formatter.rb
@@ -8,6 +8,32 @@ class TenjiFormatter
   end
 
   def format
-    
+    build_display_table.map { |row| row.join(' ').rstrip }.join("\n")
+  end
+
+  private
+
+  def build_display_table
+    rows =
+      to_tenji_characters.map do |characters|
+        first_row = characters[0] + characters[3]
+        second_row = characters[1] + characters[4]
+        third_row = characters[2] + characters[5]
+        [first_row, second_row, third_row]
+      end
+    rows[0].zip(*rows[1..])
+  end
+
+  def to_tenji_characters
+    read_tenji_numbers.map do |numbers|
+      default_characters = ['-', '-', '-', '-', '-', '-']
+      numbers.each { |number| default_characters[number - 1] = 'o' }
+      default_characters
+    end
+  end
+
+  def read_tenji_numbers
+    tenji_numbers = TenjiNumber.new(@letters)
+    tenji_numbers.convert_to_number
   end
 end
