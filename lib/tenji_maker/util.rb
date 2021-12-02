98a5a01f00d8b6bf1161b1f7d4630ca335909f50
Masato Mori <morimori@bpsinc.jp>
2021-12-03 08:57:04 +0900

    TenjiMaker#to_tenjiの実装
    refs: https://qiita.com/advent-calendar/2021/ruby-challenge
    
    愚直に仕様を実現する観点で実装しています。

---
+++ lib/tenji_maker/util.rb
@@ -0,0 +1,33 @@
+class TenjiMaker
+  # 点字の文字列変換などの操作を扱うモジュール
+  module Util
+    private
+
+    # 1文字分の点字バイナリをhuman readable文字列形式（改行区切りのo-表現）に変換する
+    # 例： 0b100000
+    # ->
+    # o-
+    # --
+    # --
+    def stringify_tenji(binary)
+      chars = format('%06B', binary).chars
+      "#{chars[0]}#{chars[3]}\n#{chars[1]}#{chars[4]}\n#{chars[2]}#{chars[5]}".gsub!('0', '-').gsub('1', 'o')
+    end
+
+    # 引数として渡された点字バイナリ表現配列から点字表現として結合した文字列を返す
+    # 例： [0b100000, 0b111111]
+    # ->
+    # o- oo
+    # -- oo
+    # -- oo
+    def stringify_tenjis(tenjis)
+      output = ['', '', '']
+      tenjis.map { stringify_tenji(_1) }.each do |tenji|
+        tenji.split("\n").each_with_index do |row, index|
+          output[index] << "#{row} "
+        end
+      end
+      output.map(&:strip).join("\n")
+    end
+  end
+end
