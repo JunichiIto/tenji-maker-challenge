f3b884884d117625e24431c88fcbf4ecca5423c8
akmhmgc <akimuhamaguchi@icloud.com>
2021-12-04 23:14:53 +0900

    Convert binary letter to 3-2 matrix

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -45,6 +45,8 @@ class TenjiMaker
 
   private
 
+  # NOTE: 文字から３行2列で点の有無を1,0で表現した点字用の行列を作成
+  # 例) "KI" => [[1, 0], [1, 0], [0, 1]]
   def to_tenji_binary_leter(letter)
     return IRREGULARS[letter.to_sym] if letter.match?(/W|Y|N/)
 
@@ -54,6 +56,6 @@ class TenjiMaker
     else
       consonant = letter[0]
       VOWELS[vowel.to_sym].concat(CONSONANTS[consonant.to_sym])
-    end
+    end.each_slice(2).to_a
   end
 end
