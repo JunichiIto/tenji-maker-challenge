b9b14276305c85a4a4ac5b8ba74a7285afeba287
watame <pandora.border@gmail.com>
2021-12-05 21:20:44 +0900

    fix: 使用例に誤りがあったので修正

--- lib/tenji_char_generator.rb
+++ lib/tenji_char_generator.rb
@@ -15,7 +15,7 @@ class TenjiCharGenerator
   # 点字に変換したいローマ字1文字から点字配列を取得する
   #
   # @example 'KA'を点字配列に変換する
-  #   to_tenji_char_array('tenji_char') #=> [['o', '-'], ['-', '-'], ['-', 'o']]
+  #   to_tenji_char_array('KA') #=> [['o', '-'], ['-', '-'], ['-', 'o']]
   #
   # @param [String] romaji_char 点字配列に変換するローマ字1文字('A','KA','WA' 等)
   # @return [Array] 2要素*3行の点字配列
