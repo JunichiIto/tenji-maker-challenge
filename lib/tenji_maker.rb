6c6695671d2b59f042e99fa3c3c84e694c470c17
yuuta31 <odk.hello@gmail.com>
2021-12-12 23:29:18 +0900

    Refactor logic

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -1,17 +1,57 @@
+require_relative 'tenji_maker/extend_code'
+
 class TenjiMaker
+  include ExtendDefineCode::Tenji
+
   def to_tenji(text)
     translated_words = arranged_words(text.split)
-      # translated_words
-      # ex => [[["o", "-"], ["o", "-"], ["-", "o"]], ... ]
+      # ex => [["o", "-", "o", "-", "-", "o"], ... ]
     puts_braille_by_line(translated_words)
   end
 
+  private
+
+  def arranged_words(entered_words)
+    output_words = entered_words.map do |word|
+      case word.size
+      when 1
+        stringify_character_tenji(BASE_WORD[word.upcase.to_sym])
+      when 2
+        stringify_characters_tenji(word)
+      when 3
+        # TODO
+      end
+    end
+  end
+
+  def stringify_character_tenji(binary)
+    chars = format('%06B', binary).each_char.map do |char|
+      if char == "1"
+        char.gsub!("1", 'o')
+      elsif char == "0"
+        char.gsub!("0", '-')
+      end
+    end
+  end
+
+  def stringify_characters_tenji(word)
+    word_key = word.upcase.to_sym
+    consonant_key = word[0].upcase.to_sym
+    vowel_key = word[1].upcase.to_sym
+    has_except_consonant = DEFINE_CONSONANT.dig(consonant_key, word_key)
+    return stringify_character_tenji(DEFINE_CONSONANT[consonant_key][word_key]) if has_except_consonant
+
+    stringify_character_tenji(DEFINE_CONSONANT[consonant_key] | BASE_WORD[vowel_key])
+  end
+
   def puts_braille_by_line(translated_words)
+    p translated_words
+    # TODO Fix output format
     output_text_line_1, output_text_line_2, output_text_line_3 = [], [], []
-    translated_words.each do |word|
-      output_text_line_1 << word[0].join + ' '
-      output_text_line_2 << word[1].join + ' '
-      output_text_line_3 << word[2].join + ' '
+    translated_words.each_with_index do |word, i|
+      output_text_line_1 << word[0] + word[3]
+      output_text_line_2 << word[1] + word[4]
+      output_text_line_3 << word[2] + word[5]
     end
 
     <<~TENJI.chomp
@@ -20,103 +60,8 @@ class TenjiMaker
       #{ output_text_line_3.join.strip }
     TENJI
   end
-
-  def arranged_words(entered_words)
-    words = []
-    entered_words.each do |word|
-      if word.size == 1
-        words << character(word)
-        next
-      end
-      words << multiple_characters(word)
-    end
-    return words
-  end
-
-  def multiple_characters(word)
-    split_word = word.split(//)
-    if split_word.size == 2
-      target_character = character(split_word.last)
-      change_consonant(target_character, split_word.first)
-      return target_character
-    elsif split_word.size == 2
-      # TODO 3文字のローマ字を点字に変換
-    end
-  end
-
-  def character(word)
-    case word
-    when 'A'
-      [['o','-'], ['-','-'], ['-','-']]
-    when 'I'
-      [['o','-'], ['o','-'], ['-','-']]
-    when 'U'
-      [['o','o'], ['-','-'], ['-','-']]
-    when 'E'
-      [['o','o'], ['o','-'], ['-','-']]
-    when 'O'
-      [['-','o'], ['o','-'], ['-','-']]
-    when 'N'
-      [['-','-'], ['-','o'], ['o','o']]
-    end
-  end
-
-  def change_consonant(lists, character)
-    case character
-    when 'K'
-      lists.map.with_index(1) do |vowel, index|
-        vowel[0..1] = ['-','o'] if index == 3
-      end
-    when 'S'
-      lists.map.with_index(1) do |vowel, index|
-        vowel[1] = 'o' if index == 2
-        vowel[0..1] = ['-','o'] if index == 3
-      end
-    when 'T'
-      lists.map.with_index(1) do |vowel, index|
-        vowel[1] = 'o' if index == 2
-        vowel[0..1] = ['o','-'] if index == 3
-      end
-    when 'N'
-      lists.map.with_index(1) do |vowel, index|
-        vowel[0..1] = ['o','-'] if index == 3
-      end
-    when 'H'
-      lists.map.with_index(1) do |vowel, index|
-        vowel[0..1] = ['o','o'] if index == 3
-      end
-    when 'M'
-      lists.map.with_index(1) do |vowel, index|
-        vowel[1] = 'o' if index == 2
-        vowel[0..1] = ['o','o']  if index == 3
-      end
-    when 'R'
-      lists.map.with_index(1) do |vowel, index|
-        vowel[1] = 'o' if index == 2
-      end
-    when 'Y'
-      except_u = lists[0][0] == lists[0][1]
-      lists.map.with_index(1) do |vowel, index|
-        vowel[0..1] = ['-','o'] if index == 1
-        vowel[0..1] = ['-','o'] if index == 2 && vowel.first == 'o'
-        if index == 3 && except_u
-          vowel[0..1] = ['o','o']
-        elsif index == 3
-          vowel[0..1] = ['o','-']
-        end
-      end
-    when 'W'
-      lists.map.with_index(1) do |vowel, index|
-        vowel[0..1] = ['-','-'] if index == 1 
-        vowel[0..1] = ['-','o'] if index == 2 && vowel.first == 'o'
-        vowel[0..1] = ['o','-'] if index == 3
-      end
-    when 'N'
-      lists.map.with_index(1) do |vowel, index|
-        vowel[0..1] = ['-','-'] if index == 1 
-        vowel[0..1] = ['-','o'] if index == 2
-        vowel[0..1] = ['o','o'] if index == 3
-      end
-    end
-  end
 end
+
+a = TenjiMaker.new
+p a.to_tenji('ya yu yo')
+p a.to_tenji('a i u')
