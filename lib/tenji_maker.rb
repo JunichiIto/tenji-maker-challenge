987cbd90201c2e83b3286e9a53cab67bca9f9795
matsuken314 <kenei031455@gmail.com>
2021-12-12 14:45:21 +0900

    テストコードをすべてパスしました

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -1,34 +1,86 @@
 class TenjiMaker
-  def new
-    TenjiMaker.new
+  def to_tenji(text)
+    read_and_output(text)
+    string = @tenji_str1.rstrip + "\n" <<  @tenji_str2.rstrip + "\n" << @tenji_str3.rstrip
+    string
   end
-  def to_tenji
-
-  end
-
-  def judge_text(text,number)
+  
+  def judge_others_text(text,number)
     i = number
-    
     case text
-    when 'A'
-     @tenji_str1[i] = 'o'
-    when 'I'
-     @tenji_str1[i] = 'o'
-     @tenji_str2[i] = 'o'
-    when 'U'
-     @tenji_str1[i] = 'o'
-     @tenji_str1[i+1] = 'o'
-    when 'E'
+    when /K\D/
+      @tenji_str3[i+1] = 'o'
+    
+    when /S\D/
+      @tenji_str2[i+1] = 'o'
+      @tenji_str3[i+1] = 'o'
+    
+    when /T\D/
+      @tenji_str2[i+1] = 'o'
+      @tenji_str3[i] = 'o'
+
+    when /N\D/
+      @tenji_str3[i] = 'o'
+
+    when /H\D/
+      @tenji_str3[i] = 'o'
+      @tenji_str3[i+1] = 'o'
+    
+    when /M\D/
+      @tenji_str2[i+1] = 'o'
+      @tenji_str3[i] = 'o'
+      @tenji_str3[i+1] = 'o'
+    
+    when /R\D/
+      @tenji_str2[i+1] = 'o'
+
+    when /Y\D/
+      @tenji_str1[i+1] = 'o'
+      @tenji_str1[i] = '-'
+      @tenji_str3[i] = 'o'
+      
+    when /W\D/
+      @tenji_str1[i] = '-'
+      @tenji_str3[i] = 'o'
+
+    when 'N'
+      @tenji_str2[i+1] = 'o'
+      @tenji_str3[i] = 'o'
+      @tenji_str3[i+1] = 'o'
+    end
+  end
+  
+  def judge_mother_text(text,number)
+    i = number
+    case 
+    when text.include?('A')
+      @tenji_str1[i] = 'o'
+
+    when text.include?('I')
+      @tenji_str1[i] = 'o'
+      @tenji_str2[i] = 'o'
+
+    when text.include?('U') && !(text.include?('Y'))
+      @tenji_str1[i] = 'o'
+      @tenji_str1[i+1] = 'o'
+
+    when text.include?('E')
       @tenji_str1[i] = 'o'
       @tenji_str1[i+1] = 'o'
       @tenji_str2[i] = 'o'
-    when 'O'
+
+    when text.include?('O') && !(text.include?('Y'))
       @tenji_str1[i+1] = 'o'
       @tenji_str2[i] = 'o'
+
+    when text == 'YU'
+      @tenji_str3[i+1] = 'o'
+    when text == 'YO'
+      @tenji_str2[i+1] = 'o'
     end
   end
-  def read_text(text)  
-   # 半角空白をカウントし文字数を割り出す 
+  
+  def read_and_output(text)  
    nil_number = text.count(' ')
    text_number = nil_number + 1
    text_array = text.split
@@ -37,16 +89,10 @@ class TenjiMaker
    @tenji_str1 = one_letter * text_number
    @tenji_str2 = one_letter * text_number
    @tenji_str3 = one_letter * text_number
+
    text_array.each_with_index do |text, num|
-    judge_text(text,num*3)
+    judge_mother_text(text, num * 3)
+    judge_others_text(text, num * 3)
    end
-   puts @tenji_str1
-   puts @tenji_str2
-   puts @tenji_str3
   end
-  
-
-
-  tenji_maker = TenjiMaker.new
-  tenji_maker.read_text('A I U A A')
 end
