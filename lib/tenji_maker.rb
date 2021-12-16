737b310234cb3c4e91f90ae7c727aa9db6e2edd5
watame <pandora.border@gmail.com>
2021-12-16 23:08:27 +0900

    update: tenji_maker内の外部関数呼び出し処理を変更

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -1,13 +1,13 @@
-require_relative '../lib/tenji_char_generator'
-require_relative '../lib/tenji_handler'
+require_relative '../lib/tenji_array_generator'
+require_relative '../lib/tenji_string_generator'
 
 # 文字列を点字に変換するクラス
 class TenjiMaker
-  include TenjiHandler
 
   # コンストラクタ
   def initialize 
-    @tenji_generator = TenjiCharGenerator.new
+    @tenji_array_generator = TenjiArrayGenerator.new
+    @tenji_string_generator = TenjiStringGenerator.new
   end
 
   # 点字に変換したい文字列を取得し、点字に変換した文字列を表示する
@@ -22,9 +22,9 @@ class TenjiMaker
   # @return [String] 点字の間をスペースで区切った文字文字列
   def to_tenji(text)
     tenji_array = text.split(' ').map { |romaji_char|
-      @tenji_generator.to_tenji_char_array(romaji_char)
+      @tenji_array_generator.to_tenji_char_array(romaji_char)
     }
-    show_tenji(tenji_array)
+    @tenji_string_generator.to_tenji_string(tenji_array)
   end
 
 end
