afc33adac2144a0eb3bb0b256830cf3426e4e698
HiroyaSakamoto <sakamoto@sikmi.com>
2021-12-16 18:07:06 +0900

    seprate methods

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -1,25 +1,20 @@
 class TenjiMaker
-  ONE_LETTER = ['A', 'I', 'U', 'E', 'O', 'N']
   TENJI_INITIAL = ['-', '-', '-', '-', '-', '-']
+
   def to_tenji(text)
     split_text = text.split(' ')
-    output = []
-    split_text.length.times do
-      output << TENJI_INITIAL.dup
+
+    tenji_letters = set_tenji_initial(split_text.length)
+
+    split_text.each_with_index do |letters, index|
+      mark_tenji(letters, tenji_letters[index])
     end
-
-    split_text.each_with_index do |letter, index|
-      mark_tenji(letter, output[index])
-    end
-
-    answer = []
-    output.map{|o| answer << [o[0] + o[1], o[2] + o[3], o[4]+ o[5]]}
-
-    answer.transpose.map { |a| a.join(" ")}.join("\n")
-
+    
+    format_tenji_for_output(tenji_letters)
   end
-  def mark_tenji(letter, tenji_letter)
-    case letter
+
+  def mark_tenji(letters, tenji_letter)
+    case letters
     when 'YA'
       tenji_letter[1] = 'o'
       tenji_letter[4] = 'o'
@@ -40,9 +35,10 @@ class TenjiMaker
       tenji_letter[3] = 'o'
       tenji_letter[4] = 'o'
       tenji_letter[5] = 'o'
+      return
     end
 
-    letter.each_char do |l|
+    letters.each_char do |l|
       case l
       when 'A'
         tenji_letter[0] = 'o'
@@ -80,5 +76,19 @@ class TenjiMaker
         tenji_letter[3] = 'o'
       end
     end
+  end
+
+  def set_tenji_initial(num)
+    tenji_letters = []
+    num.times do
+      tenji_letters << TENJI_INITIAL.dup
+    end
+    tenji_letters
+  end
+
+  def format_tenji_for_output(tenji_letters)
+    tenji_letters_for_output = []
+    tenji_letters.map{|o| tenji_letters_for_output << [o[0] + o[1], o[2] + o[3], o[4]+ o[5]]}
+    tenji_letters_for_output.transpose.map { |a| a.join(" ")}.join("\n")
   end
 end
