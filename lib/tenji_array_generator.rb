3223208811ed39f725d22a700ab037d3bb5fa778
watame <pandora.border@gmail.com>
2021-12-17 23:35:56 +0900

    update: 一部クラスでしか利用しない定数はそのクラスに配置した

--- lib/tenji_array_generator.rb
+++ lib/tenji_array_generator.rb
@@ -4,6 +4,41 @@ require_relative '../lib/tenji_constant'
 class TenjiArrayGenerator
   include TenjiConstant
 
+  # 母音の点字の位置
+  VOWEL_TENJI_POINT = {
+    'A' => [1],
+    'I' => [1, 2],
+    'U' => [1, 4],
+    'E' => [1, 2, 4],
+    'O' => [2, 4]
+  }.freeze
+
+  # 子音の点字の位置
+  CONSONANT_TENJI_POINT = {
+    'K' => [6],
+    'S' => [5, 6],
+    'T' => [3, 5],
+    'N' => [3],
+    'H' => [3, 6],
+    'M' => [3, 5, 6],
+    'R' => [5]
+  }.freeze
+
+  # 例外として処理する点字の位置
+  EXCEPTION_TENJI_POINT = {
+    'YA' => [3, 4],
+    'YU' => [3, 4, 6],
+    'YO' => [3, 4, 5],
+    'WA' => [3],
+    'N' => [3, 5, 6]
+  }.freeze
+
+  # 点字->BITの変換定義
+  TENJI_BIT = {
+    'o' => 0b1,
+    '-' => 0b0,
+  }.freeze
+
   # 点字に変換したいローマ字1文字から点字配列を取得する
   #
   # @example 'KA'を点字配列に変換する
