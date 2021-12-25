4dc9b02e78243a5ee094d2076bee0fdc2cf01321
akmhmgc <akimuhamaguchi@icloud.com>
2021-12-25 18:30:58 +0900

    Read consntants as module

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -1,6 +1,8 @@
 require 'constants'
 
 class TenjiMaker
+  include Constants
+
   def to_tenji(text)
     letters = text.split(' ')
     tenji_binary_letters = letters.map { |letter| letter_to_tenji_binary(letter) }
@@ -11,9 +13,9 @@ class TenjiMaker
   private
 
   def matrix_to_tenji(tenji_binaries_matrix)
-    tenji_binaries_matrix.map do |binary_array|
+    tenji_binaries_matrix.map { |binary_array|
       binary_array_to_tenji_line(binary_array)
-    end.join("\n")
+    }.join("\n")
   end
 
   def binary_array_to_tenji_line(binary_array)
